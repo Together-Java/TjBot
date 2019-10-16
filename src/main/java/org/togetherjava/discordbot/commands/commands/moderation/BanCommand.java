bbf7c0f562e962715ecee953f33fc88920079efe
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-10-16 18:57:41 +0200

    Add a ban command

---
+++ src/main/java/org/togetherjava/discordbot/commands/commands/moderation/BanCommand.java
@@ -0,0 +1,74 @@
+package org.togetherjava.discordbot.commands.commands.moderation;
+
+import static de.ialistannen.commandprocrastination.parsing.defaults.IntegerParsers.intGreaterThan;
+import static de.ialistannen.commandprocrastination.parsing.defaults.StringParsers.greedyPhrase;
+import static org.togetherjava.discordbot.commands.parsers.UserParsers.userById;
+import static org.togetherjava.discordbot.commands.parsers.UserParsers.userByMention;
+
+import de.ialistannen.commandprocrastination.autodiscovery.ActiveCommand;
+import de.ialistannen.commandprocrastination.command.execution.CommandException;
+import de.ialistannen.commandprocrastination.command.tree.CommandNode;
+import de.ialistannen.commandprocrastination.parsing.ParseException;
+import java.util.List;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.User;
+import org.togetherjava.discordbot.commands.CommandContext;
+import org.togetherjava.discordbot.commands.commands.BasePrefixCommand;
+
+/**
+ * A simple ban command.
+ */
+@ActiveCommand(name = "ban", parentClass = BasePrefixCommand.class)
+public class BanCommand extends CommandNode<CommandContext> {
+
+  public BanCommand() {
+    // The keyword for this command
+    super("ban");
+
+    // The command to execute
+    setCommand(this::execute);
+  }
+
+  private void execute(CommandContext context) throws ParseException {
+    JDA jda = context.getRequestContext().getChannel().getJDA();
+
+    // Uses the parser that matches, fails if none do
+    User user = context.shiftAny(List.of(
+        userById(jda),
+        userByMention(jda)
+    ));
+
+    // We do not allow 0 days
+    int durationInDays = context.shift(intGreaterThan(1));
+
+    // The reason is optional
+    String reason = context.shiftOptionally(greedyPhrase())
+        .orElse("");
+
+    // Check our privileges
+    boolean isAllowedToBan = context.getRequestContext()
+        .getGuild()
+        .getSelfMember()
+        .hasPermission(Permission.BAN_MEMBERS);
+
+    if (!isAllowedToBan) {
+      throw new CommandException("I do not have the rights to ban a user :(");
+    }
+
+    // Ban and handle success/error
+    context.getRequestContext().getGuild().ban(
+        user, durationInDays, reason
+    ).queue(
+        success ->
+            context.getRequestContext().getChannel().sendMessage(
+                "Banned user " + user.getAsMention() + " for " + durationInDays + " days due to '"
+                    + reason + "'!"
+            ).queue(),
+        error ->
+            context.getRequestContext().getChannel().sendMessage(
+                "Failed to ban user " + user.getAsMention() + " due to " + error.getMessage()
+            ).queue()
+    );
+  }
+}
---
+++ src/main/java/org/togetherjava/discordbot/commands/parsers/UserParsers.java
@@ -0,0 +1,69 @@
+package org.togetherjava.discordbot.commands.parsers;
+
+import de.ialistannen.commandprocrastination.parsing.AtomicParser;
+import de.ialistannen.commandprocrastination.parsing.ParseException;
+import de.ialistannen.commandprocrastination.util.StringReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.User;
+
+/**
+ * Contains parsers for JDA {@link User}s.
+ */
+public class UserParsers {
+
+  /**
+   * Parses a user by their id.
+   *
+   * @param jda the jda instance to use
+   * @return the parser
+   */
+  public static AtomicParser<User> userById(JDA jda) {
+    Pattern pattern = Pattern.compile("\\d+");
+    return input -> {
+      String id = input.readRegex(pattern);
+      return toUserFromId(jda, input, id);
+    };
+  }
+
+  /**
+   * Parses a user by their id.
+   *
+   * @param jda the jda instance to use
+   * @return the parser
+   */
+  public static AtomicParser<User> userByMention(JDA jda) {
+    Pattern pattern = Pattern.compile("<@(\\d+)>");
+
+    return input -> {
+      String nextWord = input.readRegex(pattern);
+
+      Matcher matcher = pattern.matcher(nextWord);
+
+      if (!matcher.find()) {
+        throw new ParseException(input, "Expected an id of the format <@id>");
+      }
+
+      return toUserFromId(jda, input, matcher.group(1));
+    };
+  }
+
+  private static User toUserFromId(JDA jda, StringReader input, String id) throws ParseException {
+    if (id.isBlank()) {
+      throw new ParseException(input, "Id may not be empty");
+    }
+
+    User user;
+    try {
+      user = jda.getUserById(id);
+    } catch (IllegalArgumentException e) {
+      throw new ParseException(input, "Invalid id: " + e.getMessage());
+    }
+
+    if (user == null) {
+      throw new ParseException(input, "User not found!");
+    }
+    return user;
+  }
+}
