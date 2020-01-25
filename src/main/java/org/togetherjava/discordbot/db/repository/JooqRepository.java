77bdbf266d50a519d03efd5b4a33f13f026849b7
Anthony Cathers <acathers@hawaii.edu>
2020-01-25 10:42:52 -1000

    #9 Database setup, apply requested changes

--- src/main/java/org/togetherjava/discordbot/db/repository/JooqRepository.java
+++ src/main/java/org/togetherjava/discordbot/db/repository/JooqRepository.java
@@ -4,11 +4,11 @@ import org.jooq.*;
 import org.jooq.impl.DSL;
 import org.togetherjava.discordbot.commands.CommandContext;
 
-public abstract class SimpleRepository<T> implements Repository<T> {
+public abstract class JooqRepository<T> implements Repository<T> {
 
     protected DSLContext dslContext;
 
-    public SimpleRepository(CommandContext context) {
+    public JooqRepository(CommandContext context) {
         dslContext = DSL.using(context.getConfig().getDburl());
     }
 
