62c82191c0091cd401c571eb02e3c731c5ffb73a
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-03-23 20:23:56 +0100

    command: Respond to direct messages

--- src/main/java/org/togetherjava/discordbot/commands/CommandListener.java
+++ src/main/java/org/togetherjava/discordbot/commands/CommandListener.java
@@ -11,6 +11,7 @@ import javax.inject.Singleton;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.togetherjava.discordbot.commands.CommandContext.JdaRequestContext;
+import org.togetherjava.discordbot.util.Messages;
 
 /**
  * A command listener for JDA. It is responsible for listening to messages an invoking commands.
@@ -18,16 +19,19 @@ import org.togetherjava.discordbot.commands.CommandContext.JdaRequestContext;
 @Singleton
 public class CommandListener extends ListenerAdapter {
 
-  private JdaExecutor executor;
+  private final JdaExecutor executor;
+  private final Messages messages;
 
   /**
    * Creates a new command listener.
    *
    * @param executor the jda executor
+   * @param messages the messages container
    */
   @Inject
-  public CommandListener(JdaExecutor executor) {
+  public CommandListener(JdaExecutor executor, Messages messages) {
     this.executor = executor;
+    this.messages = messages;
   }
 
   @Override
@@ -44,6 +48,9 @@ public class CommandListener extends ListenerAdapter {
       );
       executor.execute(content, context);
     } catch (CommandNotFoundException | NoSeparatorException ignored) {
+      if (event.getMessage().getMentionedUsers().contains(event.getJDA().getSelfUser())) {
+        event.getChannel().sendMessage(messages.tr("commands.on-mention.response")).queue();
+      }
     } catch (ParseException | AbnormalCommandResultException | CommandException e) {
       if (e.getCause() != null && e.getCause().getMessage() != null) {
         event.getChannel()
